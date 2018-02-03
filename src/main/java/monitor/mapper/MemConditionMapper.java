8db37d8a5795094deafadf736645efb5055169da
LeoPhong <jack.feng.liu@gmail.com>
2018-02-03 22:14:58 +0800

    增加了一个用于展示当前内存状态及其趋势的service

--- src/main/java/monitor/mapper/MemConditionMapper.java
+++ src/main/java/monitor/mapper/MemConditionMapper.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Result;
 import org.apache.ibatis.annotations.Results;
 
@@ -32,6 +33,17 @@ public interface MemConditionMapper {
         @Result(property="swapUsed", column="swapused", javaType=Long.class),
     })
     MemConditionEntity getLastOneById(int id);
+
+    @Select("SELECT * FROM (SELECT * FROM  mem_condition WHERE id=#{id} AND time>#{startTime} ORDER BY RAND() LIMIT 1000) AS temp_table ORDER BY time")
+    @Results({
+        @Result(property="curTime", column="time", javaType=Timestamp.class),
+        @Result(property="id", column="id", javaType=Integer.class),
+        @Result(property="memSum", column="memsum", javaType=Long.class),
+        @Result(property="memUsed", column="memused", javaType=Long.class),
+        @Result(property="swapSum", column="swapsum", javaType=Long.class),
+        @Result(property="swapUsed", column="swapused", javaType=Long.class),
+    })
+    List<MemConditionEntity>getByDuringTime(@Param("id")int id, @Param("startTime")Timestamp startTime);
 
     @Insert("INSERT INTO mem_condition VALUES(#{curTime}, #{id}, #{memUsed}, #{memSum}, #{swapUsed}, #{swapSum})")
     void insert(MemConditionEntity diskConditionEntity);
---
+++ src/main/java/monitor/service/MemService.java
@@ -0,0 +1,96 @@
+package monitor.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import java.sql.Timestamp;
+import java.util.HashMap;
+import java.util.List;
+import monitor.entity.MemConditionEntity;
+import monitor.mapper.InfoMapper;
+import monitor.mapper.MemConditionMapper;
+
+public class MemService {
+    private InfoMapper infoMapper;
+    private MemConditionMapper memConditionMapper;
+
+    @Autowired
+    public MemService(InfoMapper infoMapper, MemConditionMapper memConditionMapper) {
+        this.infoMapper = infoMapper;
+        this.memConditionMapper = memConditionMapper;
+    }
+
+    public HashMap<Timestamp, Long>getMemUsed(String hostname, long duringMin) {
+        HashMap<Timestamp, Long> mem_used = new HashMap<Timestamp, Long>();
+        int server_id = infoMapper.getByName(hostname).getId();
+        if(duringMin == 0) {
+            MemConditionEntity memConditionEntity = memConditionMapper.getLastOneById(server_id);
+            mem_used.put(memConditionEntity.getCurTime(), memConditionEntity.getMemUsed());
+        }
+        else{
+            Timestamp start_time = new Timestamp(System.currentTimeMillis() - duringMin*60*1000);
+            List<MemConditionEntity> mem_stat_list = memConditionMapper.getByDuringTime(server_id, start_time);
+            for(int index = 0; index < mem_stat_list.size(); index ++) {
+                Timestamp time_point = mem_stat_list.get(index).getCurTime();
+                long mem_value = mem_stat_list.get(index).getMemUsed();
+                mem_used.put(time_point, mem_value);
+            }
+        }
+        return mem_used;
+    }
+
+    public HashMap<Timestamp, Long>getMemSum(String hostname, long duringMin) {
+        HashMap<Timestamp, Long>mem_sum = new HashMap<Timestamp, Long>();
+        int server_id = infoMapper.getByName(hostname).getId();
+        if(duringMin == 0) {
+            MemConditionEntity memConditionEntity = memConditionMapper.getLastOneById(server_id);
+            mem_sum.put(memConditionEntity.getCurTime(), memConditionEntity.getMemSum());
+        }
+        else {
+            Timestamp start_time = new Timestamp(System.currentTimeMillis() - duringMin*60*1000);
+            List<MemConditionEntity>mem_stat_list = memConditionMapper.getByDuringTime(server_id, start_time);
+            for(int index = 0; index < mem_stat_list.size(); index++) {
+                Timestamp time_point = mem_stat_list.get(index).getCurTime();
+                long mem_value = mem_stat_list.get(index).getMemSum();
+                mem_sum.put(time_point, mem_value);
+            }
+        }
+        return mem_sum;
+    }
+
+    public HashMap<Timestamp, Long>getSwapUsed(String hostname, long duringMin) {
+        HashMap<Timestamp, Long> swap_used = new HashMap<Timestamp, Long>();
+        int server_id = infoMapper.getByName(hostname).getId();
+        if(duringMin == 0) {
+            MemConditionEntity memConditionEntity = memConditionMapper.getLastOneById(server_id);
+            swap_used.put(memConditionEntity.getCurTime(), memConditionEntity.getSwapUsed());
+        }
+        else{
+            Timestamp start_time = new Timestamp(System.currentTimeMillis() - duringMin*60*1000);
+            List<MemConditionEntity> swap_stat_list = memConditionMapper.getByDuringTime(server_id, start_time);
+            for(int index = 0; index < swap_stat_list.size(); index ++) {
+                Timestamp time_point = swap_stat_list.get(index).getCurTime();
+                long swap_value = swap_stat_list.get(index).getSwapUsed();
+                swap_used.put(time_point, swap_value);
+            }
+        }
+        return swap_used;
+    }
+
+    public HashMap<Timestamp, Long>getSwapSum(String hostname, long duringMin) {
+        HashMap<Timestamp, Long>swap_sum = new HashMap<Timestamp, Long>();
+        int server_id = infoMapper.getByName(hostname).getId();
+        if(duringMin == 0) {
+            MemConditionEntity memConditionEntity = memConditionMapper.getLastOneById(server_id);
+            swap_sum.put(memConditionEntity.getCurTime(), memConditionEntity.getSwapSum());
+        }
+        else {
+            Timestamp start_time = new Timestamp(System.currentTimeMillis() - duringMin*60*1000);
+            List<MemConditionEntity>swap_stat_list = memConditionMapper.getByDuringTime(server_id, start_time);
+            for(int index = 0; index < swap_stat_list.size(); index++) {
+                Timestamp time_point = swap_stat_list.get(index).getCurTime();
+                long swap_value = swap_stat_list.get(index).getSwapSum();
+                swap_sum.put(time_point, swap_value);
+            }
+        }
+        return swap_sum;
+    }
+}
